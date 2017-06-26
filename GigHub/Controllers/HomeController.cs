c82695bf4d8f03bcb10210cc48f8682cc33b7bdc
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2017-06-26 10:00:34 +0600

    Improve the look and feel of Home Page

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace GigHub.Controllers
         {
             var allUpcomingGigs = _context.Gigs.
                 Include(g => g.Artist).
+                Include(g => g.Genre).
                 Where(g => g.DateTime > DateTime.Now);
             return View(allUpcomingGigs);
         }
