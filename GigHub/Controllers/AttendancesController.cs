deb350fba6a171c7e5bfa1cd1c6a3e2ea75701ce
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2017-07-05 09:23:37 +0600

    Implement Attend to a Gig Option

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+using GigHub.Dtos;
+
+namespace GigHub.Controllers
+{
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a=>a.AttendeeId == userId && a.GigId == dto.GigId))
+            {
+                return BadRequest("User Already exists");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = User.Identity.GetUserId(),
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
